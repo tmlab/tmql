393d7075e9d5983920c65b5ae6af8c19a2dd689b
krosse <krosse@localhost>
2010-12-15 16:38:59 +0100

    add functions fn:get-characteristic-types and fn:get-topic-types

--- tmql4j-majortom/src/main/java/de/topicmapslab/tmql4j/majortom/extension/MajortomFunctions.java
+++ tmql4j-majortom/src/main/java/de/topicmapslab/tmql4j/majortom/extension/MajortomFunctions.java
@@ -10,12 +10,14 @@ import de.topicmapslab.tmql4j.extension.ILanguageExtension;
 import de.topicmapslab.tmql4j.grammar.lexical.IToken;
 import de.topicmapslab.tmql4j.grammar.productions.IExpression;
 import de.topicmapslab.tmql4j.majortom.grammar.functions.GetBestLabel;
+import de.topicmapslab.tmql4j.majortom.grammar.functions.GetCharacteristicTypes;
 import de.topicmapslab.tmql4j.majortom.grammar.functions.GetCoordinatesInDistance;
 import de.topicmapslab.tmql4j.majortom.grammar.functions.GetDates;
 import de.topicmapslab.tmql4j.majortom.grammar.functions.GetDatesAfter;
 import de.topicmapslab.tmql4j.majortom.grammar.functions.GetDatesBefore;
 import de.topicmapslab.tmql4j.majortom.grammar.functions.GetDatesInRange;
 import de.topicmapslab.tmql4j.majortom.grammar.functions.GetDistance;
+import de.topicmapslab.tmql4j.majortom.grammar.functions.GetTopicTypes;
 import de.topicmapslab.tmql4j.path.grammar.productions.FunctionInvocation;
 
 public class MajortomFunctions implements ILanguageExtension {
@@ -24,41 +26,60 @@ public class MajortomFunctions implements ILanguageExtension {
 	 * {@inheritDoc}
 	 */
 	@Override
-	public boolean extendsExpressionType(Class<? extends IExpression> expressionType) {
+	public boolean extendsExpressionType(
+			Class<? extends IExpression> expressionType) {
 		return false;
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public Class<? extends IExpression> getExpressionType() {		
+	public Class<? extends IExpression> getExpressionType() {
 		return FunctionInvocation.class;
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-	public void registerExtension(ITMQLRuntime runtime) throws TMQLExtensionRegistryException {
+	public void registerExtension(ITMQLRuntime runtime)
+			throws TMQLExtensionRegistryException {
 
 		/*
-		 * geographical-functions 
+		 * geographical-functions
 		 */
-		runtime.getLanguageContext().getFunctionRegistry().registerFunction(GetCoordinatesInDistance.GetCoordinatesInDistanceIdentifier, GetCoordinatesInDistance.class);
-		runtime.getLanguageContext().getFunctionRegistry().registerFunction(GetDistance.GetDistanceIdentifier, GetDistance.class);
+		runtime.getLanguageContext().getFunctionRegistry().registerFunction(
+				GetCoordinatesInDistance.GetCoordinatesInDistanceIdentifier,
+				GetCoordinatesInDistance.class);
+		runtime.getLanguageContext().getFunctionRegistry().registerFunction(
+				GetDistance.GetDistanceIdentifier, GetDistance.class);
 		/*
 		 * temporal-functions
 		 */
-		runtime.getLanguageContext().getFunctionRegistry().registerFunction(GetDates.GetDatesIdentifier, GetDates.class);
-		runtime.getLanguageContext().getFunctionRegistry().registerFunction(GetDatesAfter.GetDatesAfterIdentifier, GetDatesAfter.class);
-		runtime.getLanguageContext().getFunctionRegistry().registerFunction(GetDatesBefore.GetDatesBeforeIdentifier, GetDatesBefore.class);
-		runtime.getLanguageContext().getFunctionRegistry().registerFunction(GetDatesInRange.GetDatesInRangeIdentifier, GetDatesInRange.class);
+		runtime.getLanguageContext().getFunctionRegistry().registerFunction(
+				GetDates.GetDatesIdentifier, GetDates.class);
+		runtime.getLanguageContext().getFunctionRegistry().registerFunction(
+				GetDatesAfter.GetDatesAfterIdentifier, GetDatesAfter.class);
+		runtime.getLanguageContext().getFunctionRegistry().registerFunction(
+				GetDatesBefore.GetDatesBeforeIdentifier, GetDatesBefore.class);
+		runtime.getLanguageContext().getFunctionRegistry().registerFunction(
+				GetDatesInRange.GetDatesInRangeIdentifier,
+				GetDatesInRange.class);
 		/*
 		 * best label function
 		 */
-		runtime.getLanguageContext().getFunctionRegistry().registerFunction(GetBestLabel.GETBESTLABEL, GetBestLabel.class);
+		runtime.getLanguageContext().getFunctionRegistry().registerFunction(
+				GetBestLabel.GETBESTLABEL, GetBestLabel.class);
+		/*
+		 * index methods
+		 */
+		runtime.getLanguageContext().getFunctionRegistry().registerFunction(
+				GetCharacteristicTypes.GetCharacteristicTypes,
+				GetCharacteristicTypes.class);
+		runtime.getLanguageContext().getFunctionRegistry().registerFunction(
+				GetTopicTypes.GetTopicTypes, GetTopicTypes.class);
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
